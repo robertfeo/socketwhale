68e02e99da9bfec56271603197fa7116068bcc1d
Robert B. Fesko <fesko.robert@gmail.com>
2024-03-14 09:04:53 +0000

    Add Socket.io server and dependencies

---
+++ websocket/websocket.js
@@ -0,0 +1,14 @@
+// 1. Package
+import * as http from "http";
+import { Server } from "socket.io";
+
+// 2. Import
+const app = new Express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+// 3. Serving HTML file
+
+// 4. Define a connection event handler
+
