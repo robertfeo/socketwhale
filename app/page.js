8aad3b7093786d47c6eeb26c3e464f25eff9d1ed
Robert B. Fesko <fesko.robert@gmail.com>
2024-03-14 12:16:19 +0000

    Update Tailwind CSS configuration and package dependencies

--- app/page.js
+++ app/page.js
@@ -1,8 +1,105 @@
+"use client"
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { io } from "socket.io-client";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center m-10 min-h-svh">
-      <h1 className="text-pretty text-zinc-600 text-lg">Welcome to Socket.io Demo</h1>
-    </div>
-  );
+
+    const [chatMessage, setChatMessage] = useState("");
+
+    const socket = io("http://localhost:3001", {
+        autoConnect: false,
+    });
+
+    useEffect(() => {
+        if (!socket.connected) {
+            toast("Not connected to the server", {
+                icon: '❌',
+            });
+        }
+    }, [socket]);
+
+    socket.on("message", (message) => {
+        console.log(`Message from the client: ${message}`);
+    });
+
+    socket.on("disconnect", () => {
+        toast.success("Disconnected from client", {
+            icon: '⚡',
+        });
+    });
+
+    const handleConnect = () => {
+        if (socket.connected) {
+            toast("Already connected", {
+                icon: 'ℹ️',
+            });
+            return;
+        }
+
+        socket.connect();
+
+        const connectTimeout = setTimeout(() => {
+            if (!socket.connected) {
+                toast.error("Failed to connect to the server, please try again", {
+                    icon: '❌',
+                });
+            }
+        }, 5000); // waits 5 seconds to check the connection status
+
+        socket.on("connect", () => {
+            clearTimeout(connectTimeout);
+            toast.success("Connection is ready", {
+                icon: '✅',
+            });
+        });
+
+        socket.on("connect_error", (error) => {
+            clearTimeout(connectTimeout);
+            toast.error("Connection Error: " + error.message, {
+                icon: '❌',
+            });
+        });
+    };
+
+    const handleDisconnect = () => {
+        if (!socket.connected) {
+            toast("Already disconnected", {
+                icon: 'ℹ️',
+            });
+            return;
+        }
+        socket.disconnect();
+    }
+
+    const handleSubmit = () => {
+        if (chatMessage === "") {
+            toast.error("Please enter a message");
+            return;
+        };
+
+        socket.emit("message", chatMessage);
+    }
+
+    return (
+        <>
+            <div className="flex flex-col items-center m-10 min-h-svh">
+                <h1 className="text-pretty text-zinc-600 text-lg">Welcome to Socket.io Demo</h1>
+                <div className="flex flex-row gap-2">
+                    <button onClick={handleConnect} className="w-300 h-25 bg-slate-300 rounded-lg p-2 m-5">
+                        Connect
+                    </button>
+                    <button onClick={handleDisconnect} className="w-300 h-25 bg-slate-300 rounded-lg p-2 m-5">
+                        Disconnect
+                    </button>
+                </div>
+                <div className="flex flex-col gap-2 mt-20">
+                    <input onChange={(e) => setChatMessage(e.target.value)} type="text" className="w-400 h-200 bg-slate-100 border" />
+                    <button onClick={handleSubmit} className="w-400 h-25 bg-slate-300 rounded-lg p-2 m-5">
+                        Send
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,18 +1,39 @@
 // 1. Package
-import { http } from "http";
-import { Server } from "socket.io";
+/* const { Server } = require("socket.io"); */
 
 // 2. Import
+const util = require('util');
+const Express = require("express");
+const http = require("http");
+const { SocketAddress } = require('net');
 const app = new Express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    },
+});
 
 // 4. Define a connection event handler
+io.on("connection", (client) => {
+    console.log(`A user connected with socket ID: ${client.id} ✅`);
 
+    client.on("message", (message) => {
+        console.log(`Received message: ${message}`);
+        client.emit("message", `${message}`);
+    });
+
+    client.on("disconnect", () => {
+        console.log("A user disconnected from socket ID: " + client.id + " ❌");
+    });
+
+    client.on("connect_error", (error) => {
+        console.log(`Connection Error: ${error.message}`);
+    });
+});
 
 // 5. Listen
-server.listen(3000, () => {
-    console.log("listening on *:3000");
+server.listen(3001, () => {
+    console.log("listening on *:3001");
 });
